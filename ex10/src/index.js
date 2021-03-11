35ab51f5500da4fd678d6271891eb5fac247d0e7
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-11 21:31:55 +0100

    Added ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+          var myFirstName = "Adnan";
+          var myLastName = "Duljkovic";
+          var myHomeTown = "Sarajevo";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
