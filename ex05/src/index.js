de120d6e9c4e54c28a3a5af704e7227c6dda1cbe
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-11 19:46:03 +0100

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    // only change code below this line
+      var product = numOne * numTwo;
+      parseFloat (product);
+      parseFloat (numOne);
+      parseFloat (numTwo);
+    // Only change code above this line
+    return product;
+}
+
+console.log(main(2.5, 2.0));  // Change this line
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+module.exports = main;
